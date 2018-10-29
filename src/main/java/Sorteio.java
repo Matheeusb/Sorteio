828dd6998463f849da8bbf079e43d02f671f3509
Matheus Schroder Bereta <matheus_bereta@sicredi.com.br>
2018-10-29 11:24:35 -0300

    Ajustes de execução

--- src/main/java/Sorteio.java
+++ src/main/java/Sorteio.java
@@ -8,27 +8,6 @@ public class Sorteio extends Grupo {
     List timeAmarelo = new ArrayList();
     List timePreto = new ArrayList();
 
-    public void sorteiaTimes() {
-        this.timeAmarelo.clear();
-        this.timePreto.clear();
-
-        separaGoleiros();
-
-    }
-
-    public void separaGoleiros() {
-        separaPosicoes();
-        Collections.shuffle(goleiros, new Random(System.currentTimeMillis()));
-        int i;
-        for(i = 0; i == goleiros.size(); i++) {
-            if(this.timeAmarelo.size() >= this.timePreto.size()) {
-                this.timePreto.add(goleiros.get(i));
-            } else {
-                this.timeAmarelo.add(goleiros.get(i));
-            }
-        }
-    }
-
     public void montaTimes(List lista) {
         Collections.shuffle(lista, new Random(System.currentTimeMillis()));
         int i;
@@ -39,5 +18,6 @@ public class Sorteio extends Grupo {
                 this.timeAmarelo.add(lista.get(i));
             }
         }
+
     }
 }
